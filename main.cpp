7d204b8736df05f816bff51adea1e78f8bec3c43
ruslan913 <ruslan.eremeev.2016@gmail.com>
2023-11-13 01:15:06 +0700

    иправление ошибок

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@
 #include <sys/stat.h>
 #include <fstream>
 #include <stdexcept>
+#include <stdio.h>
 
 using namespace std;
+
+void choice(){
+
+}
 
 class Uz {
 public:
@@ -51,138 +56,146 @@ public:
 
      }
 
-     void decod(const char* intput_name="encoded.txt", const char* output_name="output.txt") {
-         unsigned long long *alfabet = new unsigned long long[256];
-         for (int i = 0; i < 256; i++) {
-             alfabet[i] = 0;
-         }
-
-         FILE *intput_file = fopen(intput_name, "rb");
-         if (intput_file == nullptr) {
-             throw invalid_argument("File not found.");
-         }
-         unsigned char col = 0;
-         unsigned int col_letters = 0;
-         col = fgetc(intput_file);
-         if (!feof(intput_file)) {
-             col_letters = (int) col;
-         }
-
-         unsigned char character = 0;
-         for (int i = 0; i < col_letters; i++) {
-             character = fgetc(intput_file);
-             if (!feof(intput_file)) {
-                 unsigned long long col_of;
-                 fread(reinterpret_cast<char *>(&alfabet[character]), sizeof(unsigned long long), 1, intput_file);
-                 cout << character << ':' << alfabet[character] << endl;
-             } else {
-                 throw invalid_argument("Can't decompress file.");
+         void decod(const char* input_name="codetext.txt", const char* output_name="output.txt") {
+             unsigned long long *alfabet = new unsigned long long[256];
+             for (int i = 0; i < 256; i++) {
+                 alfabet[i] = 0;
              }
 
-         }
-         priority_queue<Uz, vector<Uz>, Uz> tree;
-         for (int i = 0; i < 256; i++) {
-             if (alfabet[i] != 0) {
-                 string s(1, (char) i);
-
-                 Uz new_leaf(s, alfabet[i]);
-                 tree.push(new_leaf);
+             ifstream input_file(input_name, std::ios::binary);
+             if (!input_file.is_open()) {
+                 throw invalid_argument("File not found.");
              }
-         }
-         character = 0;
-         Uz *n = builder(tree);
+             unsigned char col = 0;
+             unsigned int col_letters = 0;
+             col = input_file.get();
+             if (!input_file.eof()) {
+                 col_letters = (int) col;
+             }
 
-         FILE *output_file = fopen(output_name, "wb+");
-         Uz *nodes = n;
-         unsigned char letter = 0;
-         while (!feof(intput_file)) {
-             character = fgetc((intput_file));
-             if (!feof(intput_file)) {
-                 for (int i = 7; i > -1; i--) {
-                     if (((character >> i) & 1) == 1) {
-                         if (nodes->R == NULL) {
-                             letter = nodes->key[0];
-                             if (alfabet[letter] > 0) {
-                                 alfabet[letter]--;
-                                 fputc(letter, output_file);
-                                 nodes = n->R;
+             unsigned char character;
+             for (int i = 0; i < col_letters; i++) {
+                 character = input_file.get();
+                 if (!input_file.eof()) {
+                     unsigned long long col_of;
+//                     alfabet[character] = input_file.get(); //TODO (Считывание шапки)
+                     input_file.read((char*)alfabet[character], sizeof(alfabet[character]));
+                     cout << character << ':' << alfabet[character] << endl;
+                 } else {
+                     throw invalid_argument("Can't decompress file.");
+                 }
+
+             }
+             priority_queue<Uz, vector<Uz>, Uz> tree;
+             for (int i = 0; i < 256; i++) {
+                 if (alfabet[i] != 0) {
+                     string s(1, (char) i);
+
+                     Uz new_leaf(s, alfabet[i]);
+                     tree.push(new_leaf);
+                 }
+             }
+             character = 0;
+             Uz *n = builder(tree);
+
+             ofstream output_file(output_name, std::ios::binary);
+             Uz *nodes = n;
+             unsigned char letter = 0;
+             while (!input_file.eof()) {
+                 character =input_file.get();
+                 if (!input_file.eof()) {
+                     for (int i = 7; i > -1; i--) {
+                         if (((character >> i) & 1) == 1) {
+                             if (nodes->R == NULL) {
+                                 letter = nodes->key[0];
+                                 if (alfabet[letter] > 0) {
+                                     alfabet[letter]--;
+                                     output_file << letter;
+//                                     fputc(letter, output_file);
+                                     nodes = n->R;
+                                 }
+                             } else {
+                                 nodes = nodes->R;
                              }
-                         } else {
-                             nodes = nodes->R;
-                         }
-                     } else if (((character >> 1) & 1) == 0) {
-                         if (nodes->L == NULL) {
-                             letter = nodes->key[0];
-                             if (alfabet[letter] > 0) {
-                                 fputc(letter, output_file);
-                                 nodes = n->L;
-                                 alfabet[letter]--;
+                         } else if (((character >> 1) & 1) == 0) {
+                             if (nodes->L == NULL) {
+                                 letter = nodes->key[0];
+                                 if (alfabet[letter] > 0) {
+                                     output_file << letter;
+                                     nodes = n->L;
+                                     alfabet[letter]--;
+                                 }
+                             } else {
+                                 nodes = nodes->L;
                              }
-                         } else {
-                             nodes = nodes->L;
                          }
                      }
                  }
              }
+             input_file.close();
+             output_file.close();
          }
-         fclose(intput_file);
-         fclose(output_file);
-     }
 
-     void checker(const char* before_name="intput.txt", const char* after_name="output.txt"){
+     void checker(const char* before_name="input.txt", const char* after_name="output.txt"){
      unsigned int same = 0;
-     FILE* before_file = fopen(before_name, "rb");
-     FILE* after_file = fopen(after_name, "rb");
+     ifstream before_file(before_name, std::ios::binary);
+     ifstream after_file(after_name, std::ios::binary);
 
      unsigned char after_1 = 0;
      unsigned char before_1 = 0;
-     while (!feof(after_file) && !feof(before_file)){
-         after_1 = fgetc(after_file);
-         before_1 = fgetc(before_file);
-         if(!feof(after_file) && !feof(before_file)){
+     while (!after_file.eof() && !before_file.eof()){
+         after_1 =after_file.get();
+         before_1 =before_file.get();
+         if(!after_file.eof() && !before_file.eof()){
              if(after_1 != before_1){
                  same++;
              }
          }
      }
-     while (!feof(after_file))
+     while (!after_file.eof())
      {
-         after_1 = fgetc(after_file);
-         if(!feof(after_file)){
+         after_1 =after_file.get();
+         if(!after_file.eof()){
              same++;
          }
      }
-     while (!feof(before_file))
+     while (!before_file.eof())
      {
-         before_1 = fgetc(before_file);
-         if(!feof(after_file)){
+         before_1 =before_file.get();
+         if(!after_file.eof()){
              same++;
          }
      }
-         fclose(after_file);
-         fclose(before_file);
+         after_file.close();
+         before_file.close();
          if( same == 0) cout<<"Mister_robot_is_one_love"; else cout<< "drop_out";
  }
 
-     double Coder(const char *input_name = "text.txt", const char *output_name = "codetext.txt"){
-         FILE *intput_file = fopen("text.txt", "r");
+     double Coder(const char *input_name = "input.txt", const char *output_name = "codetext.txt"){
+         ifstream input_file("input.txt", std::ios::in);
          unsigned long long *alfavit = new unsigned long long[256];
          for (int i = 0; i < 256; i++){
              alfavit[i] = 0;
          }
-         unsigned char simvol = 0;
-         while (!feof(intput_file)){
-             simvol = fgetc(intput_file);
-             if (!feof(intput_file)){
+         if(!input_file.is_open()){
+             cout << "vi dolbayobi ne tam smotrite" << endl;
+             exit(1);
+         }
+         char simvol;
+         while (!input_file.eof()){
+             simvol =input_file.get();
+             cout<<simvol<< endl;
+             if (!input_file.eof()){
                  alfavit[simvol]++;
+
              }
 
          }
+
          for (int i = 0; i < 256; i++){
 
          }
-         fclose(intput_file);
-
+         input_file.close();
          priority_queue<Uz, vector<Uz>, Uz> leafs;
          for (int i = 0; i < 256; i++) {
              if (alfavit[i] != 0) {
@@ -200,29 +213,28 @@ public:
  for (auto pair: huffmanCode){
      cout << pair.first << " " << pair.second << '\n';
  }
- FILE *output_file = fopen(output_name, "w +");
- intput_file = fopen(input_name, "r");
+ ofstream output_file(output_name, std::ios::out);
 
+ input_file.open("input.txt", std::ios::in);
 
- simvol = 0;
  unsigned char k = 0;
  unsigned int len = 0;
  unsigned int bit_len = 0;
  unsigned char letter = 0;
  char col_letters = leafs.size();
- fputc(col_letters, output_file);
-
+ output_file<< col_letters;
  for (int i = 0; i < 256; i++){
      if (alfavit[i] != 0){
-         fputc((char) i, output_file);
-         fwrite(reinterpret_cast<const char *>(&alfavit[i]), sizeof(unsigned long long), 1, output_file);
+         output_file << (char)i;
+         output_file << alfavit[i];
      }
  }
 
- while (!feof(intput_file)){
-     simvol = fgetc(intput_file);
-     if(!feof(intput_file)){
-         string s(1, simvol);
+ while (!input_file.eof()){
+     simvol =input_file.get();
+     string s = "" + simvol;
+//     string s(1, simvol);
+     if(!input_file.eof()){
          if (bit_len + huffmanCode[s].length() <= 8){
              for (int i = 0; i < huffmanCode[s].length(); i++) {
                  letter = letter << 1 | (huffmanCode[s][i]- '0');
@@ -231,23 +243,23 @@ public:
          bit_len += huffmanCode[s].length();
      } else{
          for (int i = 0; i < 8 - bit_len; i++){
-             letter = letter << 1 | (huffmanCode[i] - '0');
+             letter = letter << 1 | (huffmanCode[s][i] - '0');
          }
-        if (huffmanCode.lenght() - bit_len >= 8) {
+        if (huffmanCode[s].length() - bit_len >= 8) {
             int i = 8 - bit_len;
-            while (i + 7 < huffmanCode[s].lenght()){
+            while (i + 7 < huffmanCode[s].length()){
                 k = 0;
                 for (int j = 0; j < 8; j++){
-                    k = k << 1 | (huffmanCode[i + j] - '0');
+                    k = k << 1 | (huffmanCode[s][i + j] - '0');
                 }
                 i += 8;
-                fputc(letter, output_file);
+                 output_file << letter;
                 letter = k;
             }
             k = 0;
             len = 0;
 
-            for (int j = i; j < huffmanCode.lenght(); i++){
+            for (int j = i; j < huffmanCode[s].length(); i++){
                 k = k << 1 | (huffmanCode[s][i] - '0');
                 len++;
             }
@@ -256,21 +268,22 @@ public:
      }
      if (bit_len == 8){
 
-         fputc(letter, output_file);
+         output_file << letter;
 
          letter = k;
          bit_len = len;
          k = 0;
          len = 0;
-     }
- } else if (bit_len < 8) {
-     letter = letter << 8 - bit_len;
-     fputc(letter, output_file);
- }
-//////
 
- fclose(intput_file);
- fclose(output_file);
+     }
+     else if  (bit_len < 8) {
+         letter = letter << (8 - bit_len);
+         output_file << letter;
+     }
+ }
+
+ input_file.close();
+ output_file.close();
 
  unsigned long long file_full_size = 0;
  unsigned long long commpres_size = 0;
@@ -286,10 +299,12 @@ public:
  } else {
      perror("stat");
  }
- cout<< "compress="<< (commpres_size + 0.0) / file_full_size*100<<"%";
+ double compress =(commpres_size + 0.0) / file_full_size*100;
+ return compress;
 }
  int main(){
-     Coder();
+     double compress = Coder();
+     cout << "compress = " << compress << " %" << endl;
      decod();
      checker();
  }
